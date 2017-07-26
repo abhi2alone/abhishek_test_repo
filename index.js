c21e976d5000908e02e8fed22e199a4b237abb5c
abhi2alone <30464132+abhi2alone@users.noreply.github.com>
2017-07-26 19:05:02 +0530

    test

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'Test The time in Los Angeles is: ' + currentTime.toString(),
+        body: 'This is my world:GETOUT: The time in Los Angeles is: ' + currentTime.toString(),
     });
 };
